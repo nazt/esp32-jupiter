bded290f3ea4177d330c5f6d3d9c43251d86d241
Nat W <nat.wrw@gmail.com>
2022-06-26 22:08:14 +0700

    Add OracleNFT

---
+++ hardhat-ts/deploy/03_Deploy_OracleNFT.ts
@@ -0,0 +1,29 @@
+import { ethers, network } from "hardhat"
+import { DeployFunction } from "hardhat-deploy/types"
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+
+const TAG = 'OracleNFT'
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const { deployments, getNamedAccounts, network } = hre
+    const { deploy } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    const args: any[] = []
+    const result = await deploy(TAG, {
+        contract: TAG,
+        from: deployer,
+        args,
+        log: true,
+    })
+
+    console.log(
+        `npx hardhat verify ${result.address} ${args
+            .map(arg => `"${arg}"`)
+            .join(" ")} --network ${network.name}`,
+    )
+
+}
+
+export default func
+func.tags = [TAG]
+
